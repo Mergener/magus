f5af0825aafb992e5f2a15fa6428cdc0fd8ad1fd
mergener <mergener.thomas@gmail.com>
2025-11-04 23:10:09 -0300

    Fix test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,17 +1,14 @@
-from common.simulation import Behaviour
+from common.simulation import Behaviour, Node
 from common.utils import overrides_method
 
 
 class CustomBehaviour(Behaviour):
-    def __init__(self):
-        super().__init__()
-
     def on_update(self, dt):
         print("I implement update!")
 
 
 def test_overrides_method():
-    cb = CustomBehaviour()
+    cb = CustomBehaviour(Node())
 
     assert overrides_method(Behaviour, cb, "on_update")
     assert not overrides_method(Behaviour, cb, "on_tick")
