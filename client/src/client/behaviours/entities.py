c9a38ef372a8ac01f2fd6e4a68ea84f1189b53b9
mergener <mergener.thomas@gmail.com>
2025-11-13 03:02:48 -0300

    Remove unnecessary imports

--- client/src/client/behaviours/entities.py
+++ client/src/client/behaviours/entities.py
@@ -1,18 +1,3 @@
-import json
-from typing import TYPE_CHECKING, Callable
-
-import pygame as pg
-
-from client.behaviours.mage import Mage
-from client.behaviours.sprite_renderer import SpriteRenderer
-from common.engine.animator import Animator
-from common.engine.assets import load_image_asset, load_node_asset
-from common.engine.behaviour import Behaviour
-from common.engine.node import Node
 from common.magus.entity_type import EntityType
-
-if TYPE_CHECKING:
-    from common.engine.animation import Animation
-
 
 NETWORK_ENTITIES_ASSETS: dict[EntityType, str] = {EntityType.MAGE: "objects/mage.json"}
--- client/src/client/behaviours/network_entity.py
+++ client/src/client/behaviours/network_entity.py
@@ -1,12 +1,4 @@
-from typing import Callable
-
-from client.behaviours.entities import NETWORK_ENTITIES_ASSETS
-from client.behaviours.mage import Mage
-from client.behaviours.sprite_renderer import SpriteRenderer
-from common.engine.animator import Animator
 from common.engine.behaviour import Behaviour
-from common.engine.node import Node
-from common.magus.entity_type import EntityType
 
 
 class NetworkEntity(Behaviour):
