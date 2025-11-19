a27a5de1c35773232653e229a36be4cb64a5289f
mergener <mergener.thomas@gmail.com>
2025-11-19 00:38:11 -0300

    Taking out the trash 2

--- common/src/common/packets.py
+++ common/src/common/packets.py
@@ -5,9 +5,6 @@ from common.binary import ByteReader, ByteWriter
 from common.network import DeliveryMode, Packet
 from game.entity_type import EntityType
 
-if TYPE_CHECKING:
-    from common.behaviours.network_behaviour import SyncVarType
-
 
 class EntityPacket(Packet, ABC):
     def __init__(self, id: int):
@@ -87,60 +84,6 @@ class RotationUpdate(EntityPacket):
     @property
     def delivery_mode(self) -> DeliveryMode:
         return DeliveryMode.RELIABLE
-
-
-class SyncVarPacket(EntityPacket):
-    write_var = [
-        # Ordering based on SyncVarType enum on network_behaviour.py
-        lambda x, writer: writer.write_int8(x),
-        lambda x, writer: writer.write_int16(x),
-        lambda x, writer: writer.write_int32(x),
-        lambda x, writer: writer.write_int64(x),
-        lambda x, writer: writer.write_uint8(x),
-        lambda x, writer: writer.write_uint16(x),
-        lambda x, writer: writer.write_uint32(x),
-        lambda x, writer: writer.write_uint64(x),
-        lambda x, writer: writer.write_float32(x),
-        lambda x, writer: writer.write_float64(x),
-        lambda x, writer: writer.write_str(x),
-    ]
-    read_var = [
-        # Ordering based on SyncVarType enum on network_behaviour.py
-        lambda reader: reader.read_int8(),
-        lambda reader: reader.read_int16(),
-        lambda reader: reader.read_int32(),
-        lambda reader: reader.read_int64(),
-        lambda reader: reader.read_uint8(),
-        lambda reader: reader.read_uint16(),
-        lambda reader: reader.read_uint32(),
-        lambda reader: reader.read_uint64(),
-        lambda reader: reader.read_float32(),
-        lambda reader: reader.read_float64(),
-        lambda reader: reader.read_str(),
-    ]
-
-    def __init__(
-        self,
-        entity_id: int,
-        var_type: SyncVarType,
-        value,
-        tick_id: int,
-        reliability: DeliveryMode = DeliveryMode.RELIABLE,
-    ):
-        super().__init__(entity_id)
-        self.var_type = var_type
-        self.reliability = reliability
-        self.value = value
-
-    def on_write(self, writer: ByteWriter):
-        SyncVarPacket.write_var[cast(int, self.var_type.value)](self.value, writer)
-
-    def on_read(self, reader: ByteReader):
-        self.value = SyncVarPacket.read_var[cast(int, self.var_type.value)](reader)
-
-    @property
-    def delivery_mode(self) -> DeliveryMode:
-        return self.reliability
 
 
 class CreateEntity(Packet):
